df8040283a33e780be334f541cda36c6f1201f0f
kevinchikitcheung@gmail.com <kevinchikitcheung@gmail.com>
2021-09-14 17:21:15 -0500

    test commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
 let jsonParser = bodyParser.json();
 
@@ -10,12 +10,18 @@ const port = 3001;
 const { text } = bodyParser;
 const { Schema } = mongoose;
 
+app.use(
+  cors({
+    origin: "http://localhost:8080",
+  })
+);
+
 app.use(jsonParser);
 
 mongoose.connect("mongodb://localhost:27017/test", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
+});
 // }).then(() => {
 //     console.log('MONGOOSE SUCCESS!!!!');
 // }).catch((error) => {
@@ -23,70 +29,80 @@ mongoose.connect("mongodb://localhost:27017/test", {
 // });
 
 const testSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    DOB: Date
+  name: String,
+  email: String,
+  DOB: Date,
 });
 
-const TestModel = mongoose.model('test', testSchema);
+const TestModel = mongoose.model("test", testSchema);
 
 app.use(express.json());
 
+app.post("/testPost", (req, res) => { // .save() - SAVE ONE DOCUMENT TO MONGODB COLLECTION
+  // const test = new TestModel(req.body);
+  // }).catch((err) => {
+  //     res.status(400).send(err);
+  // })
 
-app.post('/testPost', (req, res) => {
-    // .save() - SAVE ONE DOCUMENT TO MONGODB COLLECTION
-    // const test = new TestModel(req.body);
-    // test.save().then((test) => {
-    //     res.status(201).send(test)
-    // }).catch((err) => {
-    //     res.status(400).send(err);
-    // })
+  // .create() - SAVES ONE OR MORE DOCUMENTS TO DB AND RUNS .save() FOR EVERY DOC IN DOCS
+  // TestModel.create(req.body).then((test) => {
+  //     res.status(201).send(test);
+  // }).catch((err) => {
+  //     res.status(400).send(err);
+  // })
 
-    // .create() - SAVES ONE OR MORE DOCUMENTS TO DB AND RUNS .save() FOR EVERY DOC IN DOCS
-    // TestModel.create(req.body).then((test) => {
-    //     res.status(201).send(test);
-    // }).catch((err) => {
-    //     res.status(400).send(err);
-    // })
-
-    // .insertMany() - FASTER THAN .create() BECAUSE IT ONLY SENDS ONE OPERATION TO SERVER AND NOT MANY FOR EACH DOCUMENT
-    TestModel.insertMany(req.body).then((tests) => {
-        res.status(201).send(tests);
-    }).catch((err) => {
-        res.status(400).send(err);
+  // .insertMany() - FASTER THAN .create() BECAUSE IT ONLY SENDS ONE OPERATION TO SERVER AND NOT MANY FOR EACH DOCUMENT
+  TestModel.insertMany(req.body)
+    .then((tests) => {
+      res.status(200).json(tests);
     })
-})
+    .catch((err) => {
+      console.log.log(err)
+    });
+});
 
-app.get('/testGet/:name', (req, res) => {  //POSTMAN DOES NOT NEED COLON FOR REQUESTS AND MUST BE CASE SENSITIVE
-    const {body, params} = req;
-    TestModel.find({name: params.name}).then((test) => { //NEEDED TO PUT 'PARAMS.NAME' HERE INSTEAD OF JUST 'PARAMS'
-        res.status(201).send(test);
-        console.log(req)
-    }).catch((err) => {
-        // console.log(err)
-        res.status(400).send(err);
+app.get("/testGet/:name", (req, res) => { //POSTMAN DOES NOT NEED COLON FOR REQUESTS AND MUST BE CASE SENSITIVE
+  const { body, params } = req;
+  console.log("API HIT!!!!!", params.name);
+  TestModel.find({ name: params.name })
+    .then((test) => {
+      //NEEDED TO PUT 'PARAMS.NAME' HERE INSTEAD OF JUST 'PARAMS'
+      console.log("NAME PROCESSED!!!!", test);
+      res.status(200).json(test);
+      // console.log(req)
     })
-})
+    .catch((err) => {
+      console.log(err);
+      // res.status(400).send(err);
+    });
+});
 
-app.patch('/testPatch/:name', (req, res) => { //TAKES IN NAME AND REQUESTS WILL OVERWRITE WHAT IS CURRENTLY IN DB
-    const {params} = req;
-    TestModel.updateOne({name: params.name}, req.body).then((replace) => {
-        res.status(200).send(replace);
-        console.log(req)
-    }).catch((err) => {
-        res.status(400).send(err);
+app.put("/testPatch/:name", (req, res) => {
+  //TAKES IN NAME AND REQUESTS WILL OVERWRITE WHAT IS CURRENTLY IN DB
+  const { params } = req;
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  TestModel.updateOne({ name: params.name }, req.body)
+    .then((replace) => {
+      console.log(req.body);
+      res.status(200).json(replace);
+      // console.log(req)
     })
-})
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
 
-app.delete('/testDelete/:name', (req, res) => {
-    const {params} = req;
-    TestModel.deleteOne({name: params.name}, req.body).then((del) => {
-        res.status(200).send(del);
-        console.log(params)
-    }).catch((err) => {
-        res.status(400).send(err);
+app.delete("/testDelete/:name", (req, res) => {
+  const { params } = req;
+  TestModel.deleteOne({ name: params.name }, req.body)
+    .then((del) => {
+      res.status(200).send(del);
+      console.log(params);
     })
-})
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
 
 // app.get('/api/teams/:id', (req, res) => {
 //     const {params} = req;
@@ -104,14 +120,10 @@ app.delete('/testDelete/:name', (req, res) => {
 //     res.json(body)
 // });
 
-// app.post('/post/:id', (req, res) => { 
+// app.post('/post/:id', (req, res) => {
 //     const {body, params} = req;
 //     console.log(params)
 //     res.json(body)
 // });
 
-
-
-
-
-app.listen(port, () => console.log('I LOVE YOU' + ' ' + 3001))
+app.listen(port, () => console.log("I LOVE YOU" + " " + 3001));
